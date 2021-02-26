af67027f634489969b1d14626e8f3627f4112748
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-26 01:00:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday.hash.map do |season, holiday|
+  holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
       holiday if supplies.include?("BBQ")
     end
