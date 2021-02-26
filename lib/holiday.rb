e49a89e7492c93533c89c4c4bedc2bda794ab9c7
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-26 00:16:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize!}:"
       holiday.each do |holiday, supply|
-        if holiday.to_s.find("_")
+        if holiday.to_s.include?("_")
           str_holiday = holiday.split("_").capitalize!.join(" ")
         binding.pry
       else
